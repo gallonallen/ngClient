a0d95af76ff36c38d2936b79d1face977a0771ab
Sean Casey Allen <scallen@enyalios-enterprises.com>
2017-10-03 21:35:27 -0700

    Update cli and project

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { NgClientPage } from './app.po';
+import { AppPage } from './app.po';
 
 describe('ng-client App', () => {
-  let page: NgClientPage;
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new NgClientPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
